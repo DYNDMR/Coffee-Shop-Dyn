f21d6d6d8d026240b3beeb8c55dbeeda6ded1d6f
Dayane <dayane.rodrigues@bcrcx.com>
2024-11-01 16:26:29 -0300

    Add initial structure and styling for Cafeteria Online project

---
+++ script.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector('.modal');
+const modalMask = document.querySelector('.modal-mask')
+
+function showModal(){
+    modal.style.left = "50%";
+    modalMask.style.visibility = "visible";
+}
+
+function closeModal(){
+    modal.style.left = "-30%";
+    modalMask.style.visibility = "hidden";
+}
