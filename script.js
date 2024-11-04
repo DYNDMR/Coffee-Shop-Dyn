83548b19d7c6b2a1b94c5f9f5770f4760c86eb0b
Dayane <dayane.rodrigues@bcrcx.com>
2024-11-04 14:16:20 -0300

    feat: add modal for "Learn More" section highlighting coffee quality and origins

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const modal = document.querySelector('.modal');
 const modalMask = document.querySelector('.modal-mask')
 
-function showModal(){
+function showModal() {
     modal.style.left = "50%";
     modalMask.style.visibility = "visible";
 }
 
-function closeModal(){
+function closeModal() {
     modal.style.left = "-30%";
     modalMask.style.visibility = "hidden";
+    modalAboutUs.style.left = "-50%";
 }
 
 
 function scrollToAboutSection() {
     const aboutSection = document.querySelector('.about-section');
     aboutSection.scrollIntoView({ behavior: "smooth" });
+}
+
+const modalAboutUs = document.querySelector('.container-about-us')
+
+function showModalAboutUs() {
+    modalAboutUs.style.left = "50%";
+    modalMask.style.visibility = "visible";
 }
